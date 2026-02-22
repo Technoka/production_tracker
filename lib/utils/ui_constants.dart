84c744470a33a168950561563e720e5f431a6926
David Perez <david.perez@idener.es>
2026-02-22 15:49:22 +0100

    añadido pop ups width medium

--- lib/utils/ui_constants.dart
+++ lib/utils/ui_constants.dart
@@ -12,6 +12,7 @@ class UIConstants {
   static const double SCREEN_MAX_WIDTH = 1000;
   static const int BATCH_MAX_PRODUCTS = 20;
   static const double POP_UPS_MAX_WIDTH = 250;
+  static const double POP_UPS_MAX_WIDTH_MEDIUM = 400;
 
   // Anchura de tarjetas de kanban
   static const double KANBAN_CARD_WIDTH = 280;
